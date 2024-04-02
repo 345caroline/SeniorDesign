0ca309db23a135adc5f9ea80fd9698f15d114fe9
Connal Grace <114109106+grace-connal@users.noreply.github.com>
2024-04-02 14:36:13 -0400

    Updated Server and Client

--- Bluetooth A2DP/BluetoothA2DPClient/BluetoothA2DPClient.ino
+++ Bluetooth A2DP/BluetoothA2DPClient/BluetoothA2DPClient.ino
@@ -1,40 +1,22 @@
+//Documentation for when things go wrong
+//https://pschatzmann.github.io/ESP32-A2DP/html/class_bluetooth_a2_d_p_sink.html
+
+//Taken from this example
+//https://github.com/pschatzmann/ESP32-A2DP/blob/main/examples/bt_music_receiver_and_BLE/bt_music_receiver_and_BLE.ino
+
 #include "BluetoothA2DPSink.h"
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 
-static BLEUUID serviceUUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
-static BLEUUID charUUID("beb5483e-36e1-4688-b7f5-ea07361b26a8");  
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-BluetoothA2DPSink a2dpSink;
-static boolean doConnect = false;
-static boolean connected = false;
-static boolean doScan = false;
-static BLERemoteCharacteristic* pRemoteCharacteristic;
-static BLEAdvertisedDevice* myDevice;
-std::string lastReceivedMessage = "";
-std::string value = "";
-int predictedNumber = 1;  // Start with the initial predicted number
-int receivedNumber = 0;
+BluetoothA2DPSink a2dp_sink;
+BLECharacteristic *pCharacteristic;
+char title[160] = {"Undefined"};
 
-static void notifyCallback(
-  BLERemoteCharacteristic* pBLERemoteCharacteristic,
-  uint8_t* pData,
-  size_t length,
-  bool isNotify) {
-    Serial.print("Notify callback for characteristic ");
-    Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
-    Serial.print(" of data length ");
-    Serial.println(length);
-    Serial.print("data: ");
-    Serial.println((char*)pData);
-}
-
-class MyClientCallback : public BLEClientCallbacks {
-  void onConnect(BLEClient* pclient) {
-  }
-
-  void avrc_metadata_callback(uint8_t id, const uint8_t *text) {
+void avrc_metadata_callback(uint8_t id, const uint8_t *text) {
   Serial.printf("==> AVRC metadata rsp: attribute id 0x%x, %s\n", id, text);
   if (id == ESP_AVRC_MD_ATTR_TITLE) {
     strncpy(title, (const char *)text, 160);
@@ -42,120 +24,38 @@ class MyClientCallback : public BLEClientCallbacks {
   }
 }
 
-  void onDisconnect(BLEClient* pclient) {
-    connected = false;
-    Serial.println("onDisconnect");
-  }
-};
-
-bool connectToServer() {
-  Serial.print("Forming a connection to ");
-  Serial.println(myDevice->getAddress().toString().c_str());
-
-  BLEClient* pClient = BLEDevice::createClient();
-  Serial.println(" - Created client");
-
-  pClient->setClientCallbacks(new MyClientCallback());
-
-  pClient->connect(myDevice);  
-  Serial.println(" - Connected to server");
-
-  BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
-  if (pRemoteService == nullptr) {
-    Serial.print("Failed to find our service UUID: ");
-    Serial.println(serviceUUID.toString().c_str());
-    pClient->disconnect();
-    return false;
-  }
-  Serial.println(" - Found our service");
-
-  pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
-  if (pRemoteCharacteristic == nullptr) {
-    Serial.print("Failed to find our characteristic UUID: ");
-    Serial.println(charUUID.toString().c_str());
-    pClient->disconnect();
-    return false;
-  }
-  Serial.println(" - Found our characteristic");
-
-  if(pRemoteCharacteristic->canNotify())
-      pRemoteCharacteristic->registerForNotify(notifyCallback);
-
-  connected = true;
-  return true;
+void connection_state_changed(esp_a2d_connection_state_t state, void *ptr){
+  Serial.println(a2dp_sink.to_str(state));
 }
 
-class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
-  void onResult(BLEAdvertisedDevice advertisedDevice) {
-    Serial.print("BLE Advertised Device found: ");
-    Serial.println(advertisedDevice.toString().c_str());
-
-    if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
-      BLEDevice::getScan()->stop();
-      myDevice = new BLEAdvertisedDevice(advertisedDevice);
-      doConnect = true;
-      doScan = true;
-    } 
-  }
-};
-
 void setup() {
+  // start a2dp in ESP_BT_MODE_BTDM mode
   Serial.begin(115200);
-  Serial.println("Starting Arduino BLE Client application...");
+  a2dp_sink.set_default_bt_mode(ESP_BT_MODE_BTDM);
+  a2dp_sink.set_on_connection_state_changed(connection_state_changed);
+  a2dp_sink.set_avrc_metadata_callback(avrc_metadata_callback);
+  a2dp_sink.start("A2DP Bluetooth Client");
+  Serial.println("A2DP Started!");
 
-  a2dpSink.set_default_bt_mode(ESP_BT_MODE_BTDM);
-  a2dpSink.set_arvc_metadata_callback(arvc_metadata_callback);
-  a2dpSink.start("Bluetooth A2DP Client");
+  // start BLE
+  BLEDevice::init("Bluetooth A2DP Client");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+      CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ);
 
-  BLEDevice::init("");
-
-  BLEScan* pBLEScan = BLEDevice::getScan();
-  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->setInterval(1349);
-  pBLEScan->setWindow(449);
-  pBLEScan->setActiveScan(true);
-  pBLEScan->start(5, false);
+  pCharacteristic->setValue(title);
+  pService->start();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(
+      0x06); // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
 void loop() {
-  if (doConnect == true) {
-    if (connectToServer()) {
-      Serial.println("We are now connected to the BLE Server.");
-      pinMode(2,OUTPUT);
-      digitalWrite(2,HIGH);
-    } else {
-      Serial.println("We have failed to connect to the server; there is nothing more we will do.");
-    }
-    doConnect = false;
-  }
-
-  if (connected) {
-    // Read the characteristic value from the server
-    value = pRemoteCharacteristic->readValue();
-    if(lastReceivedMessage != value.c_str()){
-    lastReceivedMessage = value;
-    // Convert the received value to an integer
-    receivedNumber = atoi(value.c_str());
-
-    // Check if the received number matches the predicted next number
-    if (receivedNumber != predictedNumber) {
-        // Print an error message and update the predicted number
-      Serial.println("Error: Connection issue may have occurred. Predicted number: " + String(predictedNumber) + ", Received number: " + String(receivedNumber));
-      predictedNumber = receivedNumber;
-      digitalWrite(2,LOW);
-    }else{
-      Serial.print("Received characteristic value from the server: ");
-      Serial.println(value.c_str());
-      digitalWrite(2,HIGH);
-      
-    }
-      predictedNumber++;
-  
-    }
-  } else if (doScan) {
-    BLEDevice::getScan()->start(0);
-  }
-  
-
-  delay(100);
+  delay(1000); // do nothing
 }
--- Bluetooth A2DP/BluetoothA2DPServer/BluetoothA2DPServer.ino
+++ Bluetooth A2DP/BluetoothA2DPServer/BluetoothA2DPServer.ino
@@ -1,16 +1,9 @@
+//Documentation for when things go wrong
+//https://pschatzmann.github.io/ESP32-A2DP/html/class_bluetooth_a2_d_p_source.html#a403b0e21db02c81b948534258e5ff37b
+
 #include "BluetoothA2DPSource.h"
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
 
-
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-  
 BluetoothA2DPSource a2dpSource;
-
-BLEServer *pServer;
-BLECharacteristic *pCharacteristic;
 
 // Taken from this example
 //https://github.com/pschatzmann/ESP32-A2DP/blob/main/examples/bt_music_sender/bt_music_sender.ino
@@ -25,19 +18,27 @@ int32_t generateSoundData(Frame *data, int32_t frameCount)
     // fill the channel data
     for (int sample = 0; sample < frameCount; ++sample) {
         float angle = pi_2 * c3_frequency * m_time + m_phase;
-        frame[sample].channel1 = m_amplitude * sin(angle);
-        frame[sample].channel2 = frame[sample].channel1;
+        data[sample].channel1 = m_amplitude * sin(angle);
+        data[sample].channel2 = data[sample].channel1;
         m_time += m_deltaTime;
     }
 
   delay(1);
 
-  return frameCount
+  return frameCount;
+}
+
+void connection_state_changed(esp_a2d_connection_state_t state, void *ptr){
+  Serial.println(a2dpSource.to_str(state));
 }
 
 void setup()
 {
-  a2dpSource.start("A2DP Bluetooth Server", generateSoundData);
+  Serial.begin(115200);
+  a2dpSource.set_local_name("A2DP Bluetooth Server");
+  a2dpSource.set_default_bt_mode(ESP_BT_MODE_BTDM);
+  a2dpSource.set_on_connection_state_changed(connection_state_changed);
+  a2dpSource.start("A2DP Bluetooth Client", generateSoundData);
 }
 
 void loop()
